b8df7ddef2d98ec54dea0087918c6d08f40fd215
CarlosPerez19 <151254507+CarlosPerez19@users.noreply.github.com>
2024-07-04 09:40:11 -0500

    Add files via upload

---
+++ Clase04/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.net.ConnectException;
+import java.sql.*;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setContentPane(new form1().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+
+    }
+}
